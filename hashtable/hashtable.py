3af6cc689785287c8304726bedb4e31d160d2fbf
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-10-29 14:07:52 -0700

    Day 1 complete

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,6 +2,7 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -22,7 +23,8 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
-
+        self.capacity = capacity
+        self.my_arr = [None] * self.capacity
 
     def get_num_slots(self):
         """
@@ -35,7 +37,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        return len(self.my_arr)
 
     def get_load_factor(self):
         """
@@ -45,7 +47,6 @@ class HashTable:
         """
         # Your code here
 
-
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
@@ -54,7 +55,7 @@ class HashTable:
         """
 
         # Your code here
-
+        pass
 
     def djb2(self, key):
         """
@@ -63,14 +64,18 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash = 5381
 
+        for x in key:
+            hash = ((hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -82,7 +87,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        self.my_arr[self.hash_index(key)] = value
 
     def delete(self, key):
         """
@@ -93,7 +98,10 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        if self.my_arr[self.hash_index(key)] is None:
+            print(f"Warning! {key} is not found")
+        else:
+            self.my_arr[self.hash_index(key)] = None
 
     def get(self, key):
         """
@@ -105,6 +113,10 @@ class HashTable:
         """
         # Your code here
 
+        if self.my_arr[self.hash_index(key)] is None:
+            print(f"Key - {key} is not found")
+        else:
+            return self.my_arr[self.hash_index(key)]
 
     def resize(self, new_capacity):
         """
@@ -114,7 +126,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        pass
 
 
 if __name__ == "__main__":
